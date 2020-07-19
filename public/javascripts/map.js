ad1b6f3e45d2a7a3d38e4a9b193c0c1698b24d62
Kevin Cueva <red1rc2@gmail.com>
2020-07-19 01:34:44 -0500

    pagina de incio

---
+++ public/javascripts/map.js
@@ -0,0 +1,14 @@
+var map = L.map('mapid').setView([-12.1247789,-77.0342339], 16);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmFkZXIwNSIsImEiOiJja2Nzb2ZybXYwazRrMnlwN3M3d3ZmNW51In0.YOWBVtgfIBNR6kIBcarxzA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
+
+L.marker([-12.1247789,-77.0342339]).addTo(map);
+L.marker([-12.1242487,-77.0344748]).addTo(map);
+L.marker([-12.1242487,-77.0344728]).addTo(map);
