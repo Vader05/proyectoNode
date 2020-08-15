90e09da2f16ba5c46ec6bf7eff9d19257c9e14ae
Kevin Cueva <red1rc2@gmail.com>
2020-08-15 00:49:55 -0500

    sendgrid agregado

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,5 +1,40 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
+
+let mailConfig;
+if (process.env.NODE_ENV === 'production') {
+   
+    const options={
+        auth:{
+            api_key: process.env.SENDGRID_API_SECRET
+        }
+    }
+    mailConfig=sgTransport(options);
+}else{
+    if (process.env.NODE_ENV === 'staging') {
+        console.log('xxxxxxxxxxxxx');
+        const options ={
+            auth:{
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig=sgTransport(options);
+    }else{
+        //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        mailConfig={
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user, 
+                pass: process.env.ethereal_pwd
+            }
+        };
+        
+    }
+}
+
+/*
 const mailConfig = {
     host: 'smtp.ethereal.email',
     port: 587 ,
@@ -8,4 +43,5 @@ const mailConfig = {
         pass:'9gugvHggbYFw7G4s7v'
     }
 };
+*/
 module.exports= nodemailer.createTransport(mailConfig);
